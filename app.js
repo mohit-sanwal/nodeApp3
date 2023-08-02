35eb96c650887f1afb2b41e7008aef7752b42322
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-02 08:23:15 +0530

    appending data to file which is getting from request

--- app.js
+++ app.js
@@ -28,7 +28,16 @@ const server = http.createServer((req, res) => {
     }
 
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.text', "this is redirected from messge route")
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.text', message);
+        })
         res.statusCode = 302;
         res.setHeader('Location', '/')
          // redirect
