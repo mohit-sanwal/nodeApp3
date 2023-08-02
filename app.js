9370b0cb12ac077dcdb6aa9721dd64f4eb1e7c66
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-02 06:46:56 +0530

    creating a server

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // this callback executes whenever a request reaches to the server
+
+
+})
+
+server.listen(3000)
+
