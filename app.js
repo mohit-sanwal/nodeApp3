49ba2da7356d3e4d9e0d5aba224c3c09906e3b5e
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-02 07:43:22 +0530

    adding checks based on route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 //    console.log(rer.url, request.method, resquest.headers);
@@ -7,6 +8,32 @@ const server = http.createServer((req, res) => {
     //  process.exit()
 
     // sending the response
+    const url = req.url;
+    const method = req.method;
+    if(url === '/') {
+        res.write(`<html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <form action="/message" method="POST">
+                <input type="text" name="message" />
+                <button type="submit">send</button>
+            </form>
+        </body>
+        </html>`)
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.text', "this is redirected from messge route")
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+         // redirect
+        return res.end();
+    }
     
     res.setHeader('content-type', 'text/html');
     res.write(`<html lang="en">
@@ -20,7 +47,7 @@ const server = http.createServer((req, res) => {
     </body>
     </html>`);
     // with single quote we have to write each tag line by line
-    
+
     res.end()
 })
 
