5f034459718527b43578b2f31bc790e7289173b7
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-02 07:05:20 +0530

    exiting the process

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+
   // this callback executes whenever a request reaches to the server
-
-
+//    console.log('req', req);
+     process.exit()
 })
 
 server.listen(3000)
